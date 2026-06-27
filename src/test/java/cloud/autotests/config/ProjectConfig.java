e8091a9fb827055e23ccc8fff08e2649611a29f5
Alexander <ale-akokhov@ayndex.ru>
2026-06-27 18:09:42 +0300

    Chrome version changed

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -12,7 +12,7 @@ public interface ProjectConfig extends Config {
 
     @DefaultValue("chrome")
     String browser();
-    @DefaultValue("128.0")
+    @DefaultValue("146.0")
     String browserVersion();
     @DefaultValue("1920x1080")
     String browserSize();
